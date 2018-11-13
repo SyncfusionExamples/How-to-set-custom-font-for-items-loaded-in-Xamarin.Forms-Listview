279232f6705f3033e3767b3c668bcae76d16e5a6
Gnana Priya <gnanapriya.namasivayam@syncfusion.com>
2018-11-13 14:43:40 +0530

    Added sample to use custom font in listview.

---
+++ CustomFontInListView/CustomFontInListView/CustomFontInListView/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Syncfusion.DataSource;
+using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfListViewSample
+{
+	  public partial class MainPage : ContentPage
+    {
+
+        #region Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            listView.DataSource.SortDescriptors.Add(new SortDescriptor()
+            {
+                PropertyName = "ContactName",
+                Direction = ListSortDirection.Ascending
+            });
+
+            listView.DataSource.GroupDescriptors.Add(new GroupDescriptor()
+            {
+                PropertyName = "ContactName",
+                KeySelector = (object obj1) =>
+                {
+                    var item = (obj1 as Contacts);
+                    return item.ContactName[0].ToString();
+                },
+            });
+
+        }
+
+        #endregion
+
+    }
+    
+}
